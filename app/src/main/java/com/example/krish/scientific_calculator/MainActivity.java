1b69d9ffdfc7db0fe9ccfd586ad819e2c9d6d881
krishna01 <krishnaqw4141@gmail.com>
2018-06-25 16:48:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/krish/scientific_calculator/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.krish.scientific_calculator;
+
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
